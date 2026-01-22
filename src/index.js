5824a69ebda11041686dd4e403887526d22cad92
Jatin Yadav <jatinrao@7458>
2026-01-22 12:22:54 +0530

    correction

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const postRoutes= require('./routes/post.routes.js')
 const app =express()
-app.use(express.json())
-const PORT =3000
+
+
 app.get('/',(req,res)=>{
     res.status(200).send("Welconme to the blogify api")
 })
+
 app.use('/api/v1/posts',postRoutes)
+
+const PORT =5000
+app.listen(PORT,()=>{
+    console.log(`serve is running at https://localhost:${PORT} `)
+})
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,7 +1,7 @@
 const express =require('express')
-const postController = require("../controllers/post.controllers.js")
+const postController = require("../controllers/post.controllers")
 const router =express.Router()
 
-router.get('/',postController.getAllPosts())
+router.get('/',postController.getAllPosts)
 
 module.exports=router
